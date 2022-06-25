941e37201f86fc920751cde0e1c256ea608b7677
Bohdan Antoshkiv <Antoshkivbl@gmail.com>
2022-06-25 13:26:49 +0300

    Закінчив 1 модуль

--- js/07-branching.js
+++ js/07-branching.js
@@ -66,3 +66,74 @@ const salary = 5000;
 
 //   console.log(a);
 // }
+
+// const message = 'Bohdan -  Antoshkiv';
+// console.log(message.split(""));
+
+// // const message = 'JavaScript - це цікаво';
+// // console.log(message.split(' '));
+
+
+// const words = ['Bohdan', 'Antoshkiv', 'is', 'ukrainian'];
+// console.log(words.join('')); // "JavaScriptцецікаво"
+// console.log(words.join(' ')); // "JavaScript це цікаво"
+// console.log(words.join('---'));
+// console.log(words.join(''));
+
+
+// const clients = ['Ivan', 'Bohdan', 'Igor', 'Petro'];
+// console.log(clients.indexOf('Bohdan')); // 2
+// console.log(clients.indexOf('Monkong'));
+// console.log(clients.indexOf('Igor'));
+// console.log(clients.indexOf('Ivan'));
+
+// const clients = ['Ivan', 'Bohdan', 'Igor', 'Petro'];
+// console.log(clients.includes('Bohdan')); // 2
+// console.log(clients.includes('Monkong'));
+// console.log(clients.includes('Igor'));
+// console.log(clients.includes('Ivan'));
+
+// // Виносимо варіанти в масив
+// const redFruits = ["Яблуко", "чорниця", "вишня", "гранат"];
+// const fruit = "вишня";
+// // Перевіряємо присутність елемента
+// const hasFruit = redFruits.includes(fruit);
+
+// if (hasFruit) {
+//   console.log(`${fruit} is a red fruit!`);
+// }
+
+// const numbers = [];
+
+// numbers.push(7);
+// console.log(numbers); // [7]
+
+// numbers.push(77);
+// console.log(numbers); // [7, 77]
+
+// numbers.push(8);
+// console.log(numbers); // [7, 77, 8]
+
+// numbers.push(88);
+// console.log(numbers); // [7, 77, 8, 88]
+
+// numbers.push(9);
+// console.log(numbers); // [7, 77, 8, 88, 9]
+
+
+const numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers.pop()); //  5
+console.log(numbers); // [1, 2, 3, 4]
+
+console.log(numbers.pop()); //  4
+console.log(numbers); // [1, 2, 3]
+
+console.log(numbers.pop()); //  3
+console.log(numbers); // [1, 2]
+
+console.log(numbers.pop()); //  2
+console.log(numbers); // [1]
+
+console.log(numbers.pop()); //  1
+console.log(numbers); // []
