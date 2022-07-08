d57eea2ed6ebae31547c1db76ad116cbfc0ab219
Bohdan Antoshkiv <Antoshkivbl@gmail.com>
2022-07-08 12:09:02 +0300

    9

--- js/03-cart.js
+++ js/03-cart.js
@@ -2,6 +2,41 @@
 
 // МОЯ ПРАКТИКА ---------РОБОТА З КОЛЕКЦІЄЮ ТОВАРІВ В КОРЗИНІ-------------------------------------------------------
 
+// { name: '🍎', price: 50 }
+// { name: '🍇', price: 70 }
+// { name: '🍋', price: 60 }
+// { name: '🍓', price: 110 }
+  
+
+
+const cart = {
+  items: [],
+  getItems() {
+    return this.items
+  },
+  add(product) {},
+  remove(productName) {},
+  clear() {},
+  countTotalPrice() {},
+  increaseQuantity(productName) {},
+  decreaseQuantity(productName) {},
+};
+
+console.table(cart.getItems());
+
+cart.add({ name: '🍎', price: 50 });
+cart.add({ name: '🍋', price: 60 });
+cart.add({ name: '🍋', price: 60 });
+cart.add({ name: '🍓', price: 110 });
+
+console.table(cart.getItems());
+
+
+
+
+
+
+/*// --------------------КОД РЕПЕТИ ----------------------------------------------------
 /*
  * Работем с коллекцией товаров в корзине:
  * - getItems()
@@ -18,37 +53,37 @@
  * { name: '🍓', price: 110 }
  */
 
-const cart = {
-  items: [],
-  getItems() {},
-  add(product) {},
-  remove(productName) {},
-  clear() {},
-  countTotalPrice() {},
-  increaseQuantity(productName) {},
-  decreaseQuantity(productName) {},
-};
+// const cart = {
+//   items: [],
+//   getItems() {},
+//   add(product) {},
+//   remove(productName) {},
+//   clear() {},
+//   countTotalPrice() {},
+//   increaseQuantity(productName) {},
+//   decreaseQuantity(productName) {},
+// };
 
-// console.table(cart.getItems());
+// // console.table(cart.getItems());
 
-cart.add({ name: '🍎', price: 50 });
-cart.add({ name: '🍋', price: 60 });
-cart.add({ name: '🍋', price: 60 });
-cart.add({ name: '🍓', price: 110 });
+// cart.add({ name: '🍎', price: 50 });
+// cart.add({ name: '🍋', price: 60 });
+// cart.add({ name: '🍋', price: 60 });
+// cart.add({ name: '🍓', price: 110 });
 
-// console.table(cart.getItems());
+// // console.table(cart.getItems());
 
-cart.remove('🍎');
-// console.table(cart.getItems());
+// cart.remove('🍎');
+// // console.table(cart.getItems());
 
-// cart.clear();
-// console.table(cart.getItems());
+// // cart.clear();
+// // console.table(cart.getItems());
 
-// cart.increaseQuantity('🍎');
-// console.table(cart.getItems());
+// // cart.increaseQuantity('🍎');
+// // console.table(cart.getItems());
 
-// cart.decreaseQuantity('🍋');
-// cart.decreaseQuantity('🍋');
-// console.table(cart.getItems());
+// // cart.decreaseQuantity('🍋');
+// // cart.decreaseQuantity('🍋');
+// // console.table(cart.getItems());
 
-// console.log('Total: ', cart.countTotalPrice());
+// // console.log('Total: ', cart.countTotalPrice());
